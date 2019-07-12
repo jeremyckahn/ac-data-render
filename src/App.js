23442dd27ca335297910cc154a61801e325faf0e
Jeremy Kahn <jeremyckahn@gmail.com>
2019-07-11 22:10:20 -0500

    Clean up fetchContacts

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ export default class App extends Component {
     this.hydrate();
   }
 
-  fetchContacts() {
-    return fetch(REACT_APP_API_URL, {
+  fetchContacts = () =>
+    fetch(REACT_APP_API_URL, {
       method: "GET",
       headers: {
+        // Ideally REACT_APP_API_KEY would be provided by some sort of session
+        // broker, but that is out of scope for this project.
         "Api-Token": REACT_APP_API_KEY,
         "x-requested-with": "xhr"
       }
     }).then(res => res.json());
-  }
 
   async hydrate() {
     const { contacts } = await this.fetchContacts();
