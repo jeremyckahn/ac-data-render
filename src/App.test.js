656f57b8b7839de6085b83a18bd71a9282ad3b55
Jeremy Kahn <jeremyckahn@gmail.com>
2019-07-11 20:42:48 -0500

    Set up basic project infrastructure

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let component;
+
+beforeEach(() => {
+  component = shallow(<App />);
+});
+
+test("boots", () => {
+  expect(component).toHaveLength(1);
 });
