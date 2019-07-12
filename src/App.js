24b3254a0cac0961e3f6ec464125399c69374a31
Jeremy Kahn <jeremyckahn@gmail.com>
2019-07-11 21:12:21 -0500

    Fetch and render raw contacts data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 import "./App.sass";
 
+const { REACT_APP_API_KEY, REACT_APP_API_URL } = process.env;
+
 export default class App extends Component {
+  state = {
+    contacts: []
+  };
+
+  constructor() {
+    super(...arguments);
+
+    this.hydrate();
+  }
+
+  fetchContacts() {
+    return fetch(REACT_APP_API_URL, {
+      method: "GET",
+      headers: {
+        "Api-Token": REACT_APP_API_KEY,
+        "x-requested-with": "xhr"
+      }
+    }).then(res => res.json());
+  }
+
+  async hydrate() {
+    const { contacts } = await this.fetchContacts();
+
+    this.setState({ contacts });
+  }
+
   render() {
+    const {
+      state: { contacts }
+    } = this;
     return (
       <div className="App">
-        <p>Hello World!</p>
+        contacts && <pre>{JSON.stringify(contacts, null, 2)}</pre>
       </div>
     );
   }
