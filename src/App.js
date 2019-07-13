54e78d33af759f5631de87ec62ad89d7f7aedbbe
Jeremy Kahn <jeremyckahn@gmail.com>
2019-07-13 13:42:20 -0500

    Render number of deals

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export default class App extends Component {
                 blank here.
                 */}
                 <td>(Unavailable)</td>
+                <td>{contact.deals.length}</td>
               </tr>
             ))}
           </tbody>
