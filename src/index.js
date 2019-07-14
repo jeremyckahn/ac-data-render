c63d69cf68bfa772fde9cf9f82f923995c502fab
Jeremy Kahn <jeremyckahn@gmail.com>
2019-07-13 19:19:15 -0500

    Fix typeface

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.sass";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.sass';
+import App from './App';
+import 'typeface-ibm-plex-sans';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
